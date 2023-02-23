d3b5e9efc7f6d260e0c50aafe956fadf08c05f18
LuisGMM <emaildemiscuentas@gmail.com>
2023-02-23 19:10:37 +0100

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -271,7 +271,7 @@ vim.g.ale_sign_warning = "W"
 vim.g.ale_echo_msg_error_str = "E"
 vim.g.ale_echo_msg_warning_str = "W"
 vim.g.ale_echo_msg_format = "[%linter%] %s [%severity%]"
-
+vim.g.ale_fix_on_save = 1
 vim.g.ale_disable_lsp = 1
 --
 
